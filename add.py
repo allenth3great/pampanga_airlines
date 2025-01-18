5fd0aff31e71511ae5fede13dd4b8fc59d20bfbf
Lennard Allen Michael Pascual <lspascual@student.hau.edu.ph>
2025-01-18 15:11:13 +0800

    Update add.py

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-flight = {}
+from flight_data import flights
 
 def add_flight():
     try:
@@ -6,12 +6,10 @@ def add_flight():
         if flight_num < 0:
             print("Flight num cannot be negative")
             return
-        destination =  input("Enter destination: ")
+        destination = input("Enter destination: ")
         seat_cap = int(input("Enter seat capacity: "))
 
-        flight[flight_num] = {"Destination": destination, "Seat Capacity": seat_cap}
+        flights[flight_num] = {"Destination": destination, "Seat Capacity": seat_cap, "Booked Seats": {}}
         print("Flight added successfully!")
     except ValueError:
         print("Invalid input. Please enter a numeric value for the flight number.")
-
-add_flight()
